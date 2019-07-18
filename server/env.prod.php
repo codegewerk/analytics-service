838fabe5e8da51005bdd80fa67b62c1477a38330
Philipp Bischoff <philippbischoff@Philipps-MacBook-Pro.local>
2019-07-18 12:05:30 +0200

    http origin for authorization

--- server/env.prod.php
+++
@@ -1,7 +0,0 @@
-<?php
-$variables = [
-    'ALLOWED_CALLING_URL' => 'https://flxbe.github.io/',
-];
-foreach ($variables as $key => $value) {
-    putenv("$key=$value");
-}
--- server/index.php
+++ server/index.php
@@ -2,28 +2,23 @@
 
 // example: https://gist.github.com/james2doyle/33794328675a6c88edd6
 
-if (file_exists('env.php')) {
-    include 'env.php';
-}
+$authorizedOrigins = include 'authorized-origins.php';
+$httpOrigin = $_SERVER['HTTP_ORIGIN'];
 
-$http_referer = $_SERVER['HTTP_REFERER'];
-$allowed_calling_url = getenv('ALLOWED_CALLING_URL');
+if (in_array($httpOrigin, $authorizedOrigins)) {
 
-if ($http_referer) {
-    if (substr($http_referer, 0, strlen($allowed_calling_url)) === $allowed_calling_url) {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type");
 
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: Content-Type");
+    $post_data = file_get_contents('php://input');
 
-        $post_data = file_get_contents('php://input');
+    $decoded_post_data = (array) json_decode($post_data);
+    print_r($decoded_post_data);
 
-        $decoded_post_data = (array) json_decode($post_data);
-        print_r($decoded_post_data);
+    file_put_contents('data.csv', dataToCsvLine($decoded_post_data), FILE_APPEND | LOCK_EX);
 
-        file_put_contents('data.csv', dataToCsvLine($decoded_post_data), FILE_APPEND | LOCK_EX);
+    http_response_code(200);
 
-        http_response_code(200);
-    }
 }
 
 http_response_code(403);
